ec4cb605a754305bc8daed5eccf7bb974e0bcb72
marigv95 <marinagvieira95@gmail.com>
2022-10-11 19:28:59 -0300

    ajuste comentarios

--- index.js
+++ index.js
@@ -1,88 +1,88 @@
-// //Exercício 01:
+//Exercício 01:
 
-// let valorGasolinaReais = parseFloat(prompt("Quanto de dinheiro gasto"));
-// let valorCompra = parseFloat(valorGasolinaReais / 6.80);
+let valorGasolinaReais = parseFloat(prompt("Quanto de dinheiro gasto"));
+let valorCompra = parseFloat(valorGasolinaReais / 6.80);
 
-// alert(`o cliente abasteceu: ${valorCompra.toFixed(2)} litros`);
-
-
-
-// //Exercício 02:
-
-// let valorCelsius = parseFloat(prompt("Valor em Celsius"));
-// let valorFahrenheit = parseFloat((9 * valorCelsius + 160) / 5 );
-
-// alert(`O valor ${valorCelsius}ºc em Fahrenheit é: ${valorFahrenheit}`);
-
-
-// //Exercício 03:
-
-// let valorGrauFarenheit = parseFloat(prompt("valor em Fahrenheit"));
-// let ValorGrauCelsius = parseFloat(((valorGrauFarenheit - 32) * 5) / 9);
-
-// alert(`O valor ${valorGrauFarenheit}ºf em Celsius é ${ValorGrauCelsius}`);
+alert(`o cliente abasteceu: ${valorCompra.toFixed(2)} litros`);
 
 
 
-// //Exercício 04:
+//Exercício 02:
 
-// let alturaCaixa = parseFloat(prompt("Altura da Caixa"));
-// let comprimentoCaixa = parseFloat(prompt("Comprimento da Caixa"));
-// let larguraCaixa = parseFloat(prompt("Largura da Caixa"));
-// let volumeCaixa = parseFloat(alturaCaixa * comprimentoCaixa * larguraCaixa);
+let valorCelsius = parseFloat(prompt("Valor em Celsius"));
+let valorFahrenheit = parseFloat((9 * valorCelsius + 160) / 5 );
 
-// alert(`O volume da caixa é ${volumeCaixa} m³⁠`);
+alert(`O valor ${valorCelsius}ºc em Fahrenheit é: ${valorFahrenheit}`);
 
 
-// //Exercício 05:
+//Exercício 03:
 
-// let valorInteiro = parseInt(prompt("Número Inteiro"));
-// let valorAoQuadrado = parseInt(valorInteiro * valorInteiro);
+let valorGrauFarenheit = parseFloat(prompt("valor em Fahrenheit"));
+let ValorGrauCelsius = parseFloat(((valorGrauFarenheit - 32) * 5) / 9);
 
-// alert(`O valor ${valorInteiro} ao quadrado é ${valorAoQuadrado}`);
-
-
-// //Exercício 06:
-
-// let valorUm = parseFloat(prompt("valor um"));
-// let valorDois = parseFloat(prompt("valor dois"));
-// let valorTres = parseFloat(prompt("valor três"));
-// let valorQuatro = parseFloat(prompt("valor quatro"));
-// let valorCinco = parseFloat(prompt("valor cinco"));
-
-// let somaValores = parseFloat(valorUm + valorDois + valorTres + valorQuatro + valorCinco);
-// let mediaValores = ((valorUm + valorDois + valorTres + valorQuatro + valorCinco) / 5);
-
-// alert(`A soma dos valores informados é: ${somaValores} e a média das notas é: ${mediaValores}`);
+alert(`O valor ${valorGrauFarenheit}ºf em Celsius é ${ValorGrauCelsius}`);
 
 
-// //Exercício 07:
 
-// let valorInteiro1 = parseInt(prompt("Primeiro valor inteiro"));
-// let valorInteiro2 = parseInt(prompt("Segundo valor inteiro"));
-// let sobraDivisao = parseFloat(valorInteiro1 % valorInteiro2);
+//Exercício 04:
 
-// alert(`O resto da divisão de ${valorInteiro1} e ${valorInteiro2} é: ${sobraDivisao}`);
+let alturaCaixa = parseFloat(prompt("Altura da Caixa"));
+let comprimentoCaixa = parseFloat(prompt("Comprimento da Caixa"));
+let larguraCaixa = parseFloat(prompt("Largura da Caixa"));
+let volumeCaixa = parseFloat(alturaCaixa * comprimentoCaixa * larguraCaixa);
 
-
-// //Exercício 08:
-
-// let caracteres = prompt("Digite seu texto");
-// let tamanho = caracteres.length;
-
-// alert(`Esse texto tem ${caracteres.length} caracteres`);
+alert(`O volume da caixa é ${volumeCaixa} m³⁠`);
 
 
-// // Exercício 09:
+//Exercício 05:
 
-// let caractereCompleto = prompt("Digite seu texto");
-// let primeiroCaractere = caractereCompleto[0]
+let valorInteiro = parseInt(prompt("Número Inteiro"));
+let valorAoQuadrado = parseInt(valorInteiro * valorInteiro);
 
-// let caractereComprimento = caractereCompleto.length;
+alert(`O valor ${valorInteiro} ao quadrado é ${valorAoQuadrado}`);
 
-// let ultimoCaractere = caractereCompleto[caractereComprimento - 1];
 
-// alert(`A primeira letra dessa frase é ${caractereCompleto[0].toUpperCase(primeiroCaractere)} e a ultima letra é ${ultimoCaractere.toLowerCase(ultimoCaractere)}`);
+//Exercício 06:
+
+let valorUm = parseFloat(prompt("valor um"));
+let valorDois = parseFloat(prompt("valor dois"));
+let valorTres = parseFloat(prompt("valor três"));
+let valorQuatro = parseFloat(prompt("valor quatro"));
+let valorCinco = parseFloat(prompt("valor cinco"));
+
+let somaValores = parseFloat(valorUm + valorDois + valorTres + valorQuatro + valorCinco);
+let mediaValores = ((valorUm + valorDois + valorTres + valorQuatro + valorCinco) / 5);
+
+alert(`A soma dos valores informados é: ${somaValores} e a média das notas é: ${mediaValores}`);
+
+
+//Exercício 07:
+
+let valorInteiro1 = parseInt(prompt("Primeiro valor inteiro"));
+let valorInteiro2 = parseInt(prompt("Segundo valor inteiro"));
+let sobraDivisao = parseFloat(valorInteiro1 % valorInteiro2);
+
+alert(`O resto da divisão de ${valorInteiro1} e ${valorInteiro2} é: ${sobraDivisao}`);
+
+
+//Exercício 08:
+
+let caracteres = prompt("Digite seu texto");
+let tamanho = caracteres.length;
+
+alert(`Esse texto tem ${caracteres.length} caracteres`);
+
+
+// Exercício 09:
+
+let caractereCompleto = prompt("Digite seu texto");
+let primeiroCaractere = caractereCompleto[0]
+
+let caractereComprimento = caractereCompleto.length;
+
+let ultimoCaractere = caractereCompleto[caractereComprimento - 1];
+
+alert(`A primeira letra dessa frase é ${caractereCompleto[0].toUpperCase(primeiroCaractere)} e a ultima letra é ${ultimoCaractere.toLowerCase(ultimoCaractere)}`);
 
 
 // Exercício 10:
